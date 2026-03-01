c14121ff24ff4d896738f2ca5e1db654e1c45ab6
Riya-Saharan <riachoudhary59@gmail.com>
2026-03-01 21:40:21 +0530

    largest element in an array

--- arrays/L17.cpp
+++ arrays/L17.cpp
@@ -30,3 +30,87 @@ int main()
 
     return 0;
 }
+
+// by using bubble sort
+for (int i = 0; i < n - 1; i++)
+{
+    for (int j = 0; j < n - i - 1; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            // swap
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
+// time complexity = o(NlogN) space complexity = o(1)
+
+// leetcode
+class Solution
+{
+public:
+    int findLargest(vector<int> &nums)
+    {
+
+        // sort the array
+        sort(nums.begin(), nums.end());
+
+        // return the last element
+        return nums[nums.size() - 1];
+    }
+};
+
+// optimal solution
+int largestElement(int arr[], int n)
+{
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // calling function
+    int result = largestElement(arr, n);
+
+    cout << "Largest element is: " << result << endl;
+    return 0;
+}
+
+// leetcode
+#include <bits/stdc++.h>
+int largestElement(vector<int> &arr, int n)
+{
+    int largest = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+// tc = o(N)
